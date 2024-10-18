67c72a07337da6ec7f631ccc7feeaef2ddd40912
MakarSux <mm.pyshkin@yandex.ru>
2024-10-18 20:03:44 +0300

    add routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+
+Route::get('/', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::post('/products/{id}/add-to-cart', [ProductController::class, 'addToCart'])->name('products.addToCart');
+
+Route::get('/cart', [OrderController::class, 'cart'])->name('orders.cart');
+Route::post('/checkout', [OrderController::class, 'checkout'])->name('orders.checkout');
+Route::get('/orders', [OrderController::class, 'orders'])->name('orders.index');

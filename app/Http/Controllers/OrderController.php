9025b1682962879c3a6b49d0bb267f25bcbfd74e
MakarSux <mm.pyshkin@yandex.ru>
2024-10-18 20:00:54 +0300

    add controllers

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function cart()
+    {
+        return view('orders.cart');
+    }
+
+    public function checkout()
+    {
+        $cart = session()->get('cart');
+        if(!$cart){
+            return redirect()->route('products.index')->with('error', 'Корзина пуста');
+        }
+
+        $total = 0;
+        foreach($cart as $item){
+            $total += $item['price'] * $item['quantity'];
+        }
+
+        $order = Order::create(['total_price' => $total]);
+
+        foreach($cart as $item){
+            $order->products()->attach($item['id'], ['quantity' => $item['quantity']]);
+        }
+
+        session()->forget('cart');
+        return redirect()->route('products.index')->with('success', 'Заказ успешно оформлен');
+    }
+
+    public function orders()
+    {
+        $orders = Order::all();
+        return view('orders.index', compact('orders'));
+    }
+
 }
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $products = Product::all();
+        return view('products.index', compact('products'));
+    }
+
+    public function addToCart(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+        $cart = session()->get('cart', []);
+
+        if(isset($cart[$id])){
+            $cart[$id]['quantity']++;
+        } else{
+            $cart[$id] = [
+                "name" => $product->name,
+                "quantity" => 1,
+                "price" => $product->price,
+                "id" => $product->id
+            ];
+        }
+
+        session()->put('cart', $cart);
+        return redirect()->back()-with('success', 'Продукт успешно добавлен в корзину');
+    }
 }
+
